278880f719dcd45b96eee4f70a25ea51a02ec1a3
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-08-07 10:11:00 -0700

    mvp complete

--- src/main/java/com/piggybank/piggybank/controllers/CoinController.java
+++ src/main/java/com/piggybank/piggybank/controllers/CoinController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -34,6 +35,34 @@ public class CoinController {
             totalCoins += (c.getValue() * c.getQuantity());
         }
         System.out.println("The piggy bank holds " + totalCoins);
+        return new ResponseEntity<>("Status OK", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/money/{amount}", produces={"application/json"})
+    public ResponseEntity<?> removeMoney (@PathVariable double amount) {
+        List<Coin> coinList = new ArrayList<>();
+        coinrepos.findAll().iterator().forEachRemaining(coinList::add);
+        coinList.sort((c1, c2) -> Double.compare( c2.getValue(), c1.getValue()));
+        double totalCoins = 0;
+        for (Coin c : coinList) {
+            totalCoins += (c.getValue() * c.getQuantity());
+        }
+
+        if (amount > totalCoins) {
+            System.out.println("Money not available");
+        }
+
+        double tempAmount;
+
+        for (Coin c : coinList) {
+            tempAmount = amount;
+            if (tempAmount > (c.getValue() * c.getQuantity())) {
+                
+            }
+        }
+
+
+        System.out.println( "The piggy bank holds $" + totalCoins);
         return new ResponseEntity<>("Status OK", HttpStatus.OK);
     }
 }
